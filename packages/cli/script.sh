cd1d2abbd6917abb7fc1266895d50af84855bdd8
ThreeAndTwo <kaiofzc@gmail.com>
2024-09-11 15:59:56 +0800

    added shell script

---
+++ packages/cli/script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# https://mempool.fractalbitcoin.io/api/v1/fees/recommended
+# bash 请求 fee 
+
+while true; do
+    response=$(curl -s https://mempool.fractalbitcoin.io/api/v1/fees/recommended)
+    fastestFee=$(echo $response | jq '.fastestFee')
+    echo $fastestFee
+
+    # 如果没有获取到 fastestFee，默认给 100
+    if [ -z "$fastestFee" ] || [ "$fastestFee" == "null" ]; then
+        fastestFee=1000
+    fi
+
+    echo $fastestFee
+    command="yarn cli mint -i 45ee725c2c5993b3e4d308842d87e973bf1951f5f7a804b21e4dd964ecd12d6b_0 5 --fee-rate $fastestFee"
+    $command
+
+    if [ $? -ne 0 ]; then
+        echo "命令执行失败，退出循环"
+        exit 1
+    fi
+
+    sleep 1
+done
