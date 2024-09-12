92567c5313a6e731669a8459ce0d86ea4e8dc5a7
69tc <696969tc@gmail.com>
2024-09-12 18:04:43 +0700

    add index

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -6,7 +6,7 @@ const express = require("express");
 
 const app = express();
 
-const port = 3000;
+const port = 4000;
 
 // import { ConfigService, WalletService } from 'src/providers';
 // import { randomBytes } from 'crypto';
