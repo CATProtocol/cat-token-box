ef41619cc3a4fcf5f938c266591a5a1787a4234d
kelvin2608 <kelvin@bvm.network>
2024-09-14 13:10:57 +0700

    update code

--- packages/cli/index.ts
+++ packages/cli/index.ts
@@ -115,7 +115,11 @@ app.get("/create-wallet", (req: any, res: any) => {
   res.status(200).json({ message: "hello" });
 });
 
+<<<<<<< HEAD
 app.pos("/kelvin", (req: any, res: any) => {
+=======
+app.post("/kelvin", (req: any, res: any) => {
+>>>>>>> 6aed216 (update code)
   res.status(200).json({ kelvin: "kelvin" });
   return;
 });
