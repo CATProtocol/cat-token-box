826f1ecb3ddac5899805dfa70ab1690b1cdf6fdf
hotbroker <50006539+hotbroker@users.noreply.github.com>
2024-10-16 20:32:33 +0800

    typo for command dataDir

--- packages/cli/src/commands/base.command.ts
+++ packages/cli/src/commands/base.command.ts
@@ -76,9 +76,9 @@ export abstract class BaseCommand extends CommandRunner {
       });
     }
 
-    if (options.datadir) {
+    if (options.dataDir) {
       Object.assign(cliConfig, {
-        dataDir: options.datadir,
+        dataDir: options.dataDir,
       });
     }
 
