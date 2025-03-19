d19eb330743a87387c0bda3dbf5cb4a54d4eb113
opcatbugcloser <opcatbugcloser@gmail.com>
2025-03-20 02:02:29 +0800

    add cat721 base test

--- packages/sdk/tests/features/cat721/send/contractSend.test.ts
+++ packages/sdk/tests/features/cat721/send/contractSend.test.ts
@@ -1,106 +1,100 @@
-// import * as dotenv from 'dotenv';
-// dotenv.config();
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-// // eslint-disable-next-line @typescript-eslint/no-unused-vars
-// import { expect, use } from 'chai';
-// import chaiAsPromised from 'chai-as-promised';
-// import { hash160, Ripemd160 } from 'scrypt-ts';
-// import { NftParallelClosedMinterCat721Meta } from '../../../../src/lib/metadata';
-// import { verifyInputSpent } from '../../../utils/txHelper';
-// import { CAT721 } from '../../../../src/contracts/nft/cat721';
-// import { NftGuard } from '../../../../src/contracts/nft/nftGuard';
-// import { testSigner } from '../../../utils/testSigner';
-// import { contractSendNft } from '../nftParallelClosedMinter.utils';
-// import { CAT721Proto } from '../../../../src/contracts/nft/cat721Proto';
-// import { CAT721Covenant } from '../../../../src/covenants/cat721Covenant';
-// import { Cat721Utxo } from '../../../../src/lib/provider';
-// import { toTokenAddress } from '../../../../src/lib/utils';
-// import { NftParallelClosedMinter, NftParallelClosedMinterCovenant } from '../../../../src';
-// import { TestNftGenerater } from '../../../utils/testNftGenerater';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { expect, use } from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import { bvmVerify, hash160, Ripemd160 } from '@scrypt-inc/scrypt-ts-btc';
+import { TestCAT721Generater } from '../../../utils/testCAT721Generater';
+import { NftParallelClosedMinterCat721Meta, toTokenAddress } from '../../../../src';
+import { CAT721Utxo } from '../../../../src/lib/provider';
+import { loadAllArtifacts } from '../utils';
+import { testSigner } from '../../../utils/testSigner';
+import { CAT721Covenant } from '../../../../src/covenants/cat721Covenant';
+import { contractSendNft } from '../../../../src/features/cat721/send/contractSend';
+import { testChainProvider, testUtxoProvider } from '../../../utils/testProvider';
+use(chaiAsPromised);
 
-// use(chaiAsPromised);
+describe('Test the feature `contractSend` for `Cat721Covenant`', () => {
+    let contractHash: Ripemd160;
+    let nftGenerater: TestCAT721Generater;
+    let metadata: NftParallelClosedMinterCat721Meta;
 
-// describe('Test the feature `contractSend` for `Cat721Covenant`', () => {
-//     let address: string;
-//     let contractScript: Ripemd160;
-//     let contractHash: Ripemd160;
-//     let nftGenerater: TestNftGenerater;
-//     let metadata: NftParallelClosedMinterCat721Meta;
+    before(async () => {
+        loadAllArtifacts();
+        const address = await testSigner.getAddress();
+        const contractScript = toTokenAddress(address);
+        contractHash = hash160(contractScript);
+        metadata = {
+            name: 'Locked-up Cats',
+            symbol: 'LCAT',
+            max: 10000n,
+            minterMd5: '',
+            description:
+                'It’s the first NFT collection distributed on the Bitcoin Network based on the brand new CAT721 protocol.',
+        };
+        nftGenerater = await TestCAT721Generater.init(metadata);
+    });
 
-//     before(async () => {
-//         await NftParallelClosedMinter.loadArtifact();
-//         await CAT721.loadArtifact();
-//         await NftGuard.loadArtifact();
-//         address = await testSigner.getAddress();
-//         contractScript = toTokenAddress(address);
-//         contractHash = hash160(contractScript);
+    const getNftUtxos = async function (generater: TestCAT721Generater, contractHash: string, n: number) {
+        const r: CAT721Utxo[] = [];
+        for (let index = 0; index < n; index++) {
+            const utxo = await generater.mintNftToHash160(contractHash);
+            r.push(utxo);
+        }
+        return r;
+    };
 
-//         metadata = {
-//             name: 'Locked-up Cats',
-//             symbol: 'LCAT',
-//             max: 10000n,
-//             minterMd5: NftParallelClosedMinterCovenant.LOCKED_ASM_VERSION,
-//             description:
-//                 'It’s the first NFT collection distributed on the Bitcoin Network based on the brand new CAT721 protocol.',
-//         };
-//         nftGenerater = await TestNftGenerater.init(metadata);
-//     });
+    describe('When sending nfts in a single tx', () => {
+        it('should contract send one nft utxo successfully', async () => {
+            const nftUtxos = await getNftUtxos(nftGenerater, contractHash, 1);
+            await testContractSendResult(nftUtxos);
+        });
 
-//     const getNftUtxos = async function (generater: TestNftGenerater, contractHash: string, n: number) {
-//         const r: Cat721Utxo[] = [];
-//         for (let index = 0; index < n; index++) {
-//             const utxo = await generater.mintNftToHash160(contractHash);
-//             r.push(utxo);
-//         }
-//         return r;
-//     };
+        it('should contract send multiple nft utxos successfully', async () => {
+            const nftUtxos = await getNftUtxos(nftGenerater, contractHash, 3);
+            await testContractSendResult(nftUtxos);
+        });
+    });
 
-//     describe('When sending nfts in a single tx', () => {
-//         it('should contract send one nft utxo successfully', async () => {
-//             const nftUtxos = await getNftUtxos(nftGenerater, contractHash, 1);
-//             await testContractSendResult(nftUtxos);
-//         });
+    async function testContractSendResult(cat721Utxos: CAT721Utxo[]) {
+        const { guardTx, sendTx } = await contractSendNft(
+            testSigner,
+            testUtxoProvider,
+            testChainProvider,
+            nftGenerater.deployInfo.minterAddr,
+            cat721Utxos,
+            cat721Utxos.map(() => contractHash),
+            await testChainProvider.getFeeRate(),
+        );
 
-//         it('should contract send multiple nft utxos successfully', async () => {
-//             const nftUtxos = await getNftUtxos(nftGenerater, contractHash, 3);
-//             await testContractSendResult(nftUtxos);
-//         });
-//     });
+        // check guard tx
+        expect(guardTx).not.to.be.undefined;
+        expect(guardTx.isFinalized).to.be.true;
 
-//     async function testContractSendResult(cat721Utxos: Cat721Utxo[]) {
-//         const { guardTx, sendTx } = await contractSendNft(
-//             nftGenerater.deployInfo.minterAddr,
-//             cat721Utxos,
-//             cat721Utxos.map(() => contractHash),
-//         );
+        // check send tx
+        expect(sendTx).not.to.be.undefined;
+        expect(sendTx.isFinalized).to.be.true;
 
-//         // check guard tx
-//         expect(guardTx).not.to.be.undefined;
-//         expect(guardTx.isFinalized).to.be.true;
+        // verify token input unlock
+        for (let i = 0; i < cat721Utxos.length; i++) {
+            expect(bvmVerify(sendTx, i)).to.be.true;
+        }
 
-//         // check send tx
-//         expect(sendTx).not.to.be.undefined;
-//         expect(sendTx.isFinalized).to.be.true;
+        // verify guard input unlock
+        expect(bvmVerify(sendTx, cat721Utxos.length)).to.be.true;
 
-//         // verify token input unlock
-//         for (let i = 0; i < cat721Utxos.length; i++) {
-//             expect(verifyInputSpent(sendTx, i)).to.be.true;
-//         }
-
-//         // verify guard input unlock
-//         expect(verifyInputSpent(sendTx, cat721Utxos.length)).to.be.true;
-
-//         // verify token to receiver
-//         for (let index = 0; index < cat721Utxos.length; index++) {
-//             const toReceiverOutputIndex = 1 + index;
-//             const toReceiverToken = new CAT721Covenant(
-//                 nftGenerater.deployInfo.minterAddr,
-//                 CAT721Proto.create(cat721Utxos[index].state.localId, contractHash),
-//             );
-//             expect(Buffer.from(sendTx.txOutputs[toReceiverOutputIndex].script).toString('hex')).to.eq(
-//                 toReceiverToken.lockingScript.toHex(),
-//             );
-//             expect(sendTx.txState.stateHashList[toReceiverOutputIndex - 1]).to.eq(toReceiverToken.stateHash);
-//         }
-//     }
-// });
+        // verify token to receiver
+        for (let index = 0; index < cat721Utxos.length; index++) {
+            const toReceiverOutputIndex = 1 + index;
+            const toReceiverToken = new CAT721Covenant(nftGenerater.deployInfo.minterAddr, {
+                localId: cat721Utxos[index].state.localId,
+                ownerAddr: contractHash,
+            });
+            expect(Buffer.from(sendTx.txOutputs[toReceiverOutputIndex].script).toString('hex')).to.eq(
+                toReceiverToken.lockingScript.toHex(),
+            );
+            expect(sendTx.getTxoStateHashes()[toReceiverOutputIndex - 1]).to.eq(toReceiverToken.stateHash);
+        }
+    }
+});
---
+++ packages/sdk/tests/features/cat721/utils.ts
@@ -0,0 +1,20 @@
+import { readArtifact } from '../../utils/index';
+import {
+    CAT721ClosedMinter,
+    CAT721,
+    CAT721StateLib,
+    CAT721Guard,
+    CAT721GuardStateLib,
+    // CAT721OpenMinter,
+} from '../../../src';
+export const loadAllArtifacts = function () {
+    //
+    CAT721ClosedMinter.loadArtifact(readArtifact('artifacts/cat721/minters/cat721ClosedMinter.json'));
+    // CAT721OpenMinter.loadArtifact(readArtifact('artifacts/cat721/minters/cat721OpenMinter.json'));
+    //
+    CAT721.loadArtifact(readArtifact('artifacts/cat721/cat721.json'));
+    CAT721StateLib.loadArtifact(readArtifact('artifacts/cat721/cat721State.json'));
+    //
+    CAT721Guard.loadArtifact(readArtifact('artifacts/cat721/cat721Guard.json'));
+    CAT721GuardStateLib.loadArtifact(readArtifact('artifacts/cat721/cat721GuardState.json'));
+};
