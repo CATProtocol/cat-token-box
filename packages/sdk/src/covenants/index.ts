5025da9dc2a5e9bb7cba0b371a3ebc309b758876
opcatbugcloser <opcatbugcloser@gmail.com>
2025-03-24 15:59:16 +0800

    export cat721 covenant

--- packages/sdk/src/covenants/index.ts
+++ packages/sdk/src/covenants/index.ts
@@ -1,3 +1,6 @@
 export * from './cat20Covenant';
 export * from './cat20GuardCovenant';
 export * from './cat20OpenMinterCovenant';
+export { CAT721Covenant } from './cat721Covenant';
+export * from './cat721GuardCovenant';
+export * from './cat721OpenMinterCovenant';
