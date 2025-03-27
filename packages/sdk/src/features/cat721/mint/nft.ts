44866b6cc8db65b2a8d6697fa889da52aae56e5b
opcatdev <opcatdev@gmail.com>
2025-03-27 17:17:34 +0800

    Fix

--- packages/sdk/src/features/cat721/mint/nft.ts
+++ packages/sdk/src/features/cat721/mint/nft.ts
@@ -1,4 +1,4 @@
-import { Postage } from '@cat-protocol/cat-sdk-v2';
+import { Postage } from '../../../';
 import { getCatNFTCommitScript } from '../../../lib/commit';
 import { ExtPsbt, hexToUint8Array, Signer, UTXO } from '@scrypt-inc/scrypt-ts-btc';
 import { catToXOnly, isP2TR, scriptToP2tr } from '../../../lib/utils';
