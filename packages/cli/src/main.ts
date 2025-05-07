185719cc2981af37b191d9e248d8afb2f8043ccf
marianjf456 <marianjf456@gmail.com>
2025-05-07 14:31:58 +0800

    Update main.ts

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -4,6 +4,7 @@ import { logerror, MinterType } from './common';
 import { OpenMinter } from '@cat-protocol/cat-smartcontracts';
 export const ArtifactsMD5 = new Map<string, MinterType>();
 
+// test
 export async function bootstrap() {
   try {
     ArtifactsMD5.set(OpenMinter.getArtifact().md5, MinterType.OPEN_MINTER);
